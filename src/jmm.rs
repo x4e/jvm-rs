68d5dfcae0c9edd51a12d7023abc2e13bc5d5326
mastercooker <c.o.okiedragon234@gmail.com>
2020-08-31 14:11:59 +0100

    Formatting

--- src/jmm.rs
+++ src/jmm.rs
@@ -1,3 +1,8 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+
 use crate::bitfield::__BindgenBitfieldUnit;
 use jni::sys::{jvalue, jstring, jlong, jobjectArray, jint, jboolean, JNIEnv, jobject, jlongArray};
 
