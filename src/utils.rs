4d59aaab89f1697fe51a17d055a4129bf5d1736a
mastercooker <c.o.okiedragon234@gmail.com>
2020-09-22 14:09:12 +0100

    fixes to macros

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,16 @@
-use std::ffi::CString;
-
 #[macro_export]
 macro_rules! cstr {
 	($s:expr) => {
-		CString::new($s).unwrap().into_raw()
+		std::ffi::CString::new($s).unwrap().into_raw()
 	};
 }
 
 #[macro_export]
 macro_rules! check_jni {
-	($e:expr) => {
+	($e:expr) => {{
 		let result = $e;
-		if result != JNI_OK as i32 {
+		if result != rs_jvm_bindings::jni::JNI_OK as u32 {
 			panic!("JNI expression failed (received {} from {})", result, stringify!($e));
 		}
-	};
+	}};
 }
